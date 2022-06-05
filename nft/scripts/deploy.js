c09246565d0d950bf88f9786786e3e0704da07cf
Jacob Alebiosu <jacobalebiosu@gmail.com>
2022-06-05 23:56:55 +0100

    debugging deployment packages

--- nft/scripts/deploy.js
+++ nft/scripts/deploy.js
@@ -7,7 +7,7 @@ const {ethers} = require("hardhat");
 async function main() {
 
     // 1. Tell the script we want to deploy the "nftee.sol" contract
-    const contract = await ethers.getContractFactory("nftee");
+    const contract = await ethers.getContractFactory("NFTee");
 
     // 2. Deploy it
     const deployedContract = await contract.deploy();
@@ -15,7 +15,7 @@ async function main() {
     await deployedContract.deployed();
     
     // 3. Print the address of the deployed contract
-    console.log("NFT Contract deployed to: ", deployedCotnract.address);
+    console.log("NFT Contract deployed to: ", deployedContract.address);
 }
 
 main()
