2628eecf246aba7402987db9feb161da8159419e
Jacob Alebiosu <jacobalebiosu@gmail.com>
2022-07-21 20:06:50 +0100

    whitelist dap

---
+++ Whitelist_Dapp/contracts/Whitelist.sol
@@ -0,0 +1,26 @@
+ //SPDX-License_Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Whitelist {
+    // maximum number of addresse which can be whitelisted
+    uint8 public maxWhitelistedAddresses;
+
+    // keep track of number of addresses whitelisted till now
+    uint8 public numAddressesWhitelisted;
+
+    mapping(address => bool) public whitelistedAddresses;
+
+    constructor(uint8 _maxWhitelistedAddresses) {
+        maxWhitelistedAddresses = _maxWhitelistedAddresses;
+    }
+
+    function addAddressToWhitelist() public {
+        // msg.sender is the address of the user who called this function
+        require(!whitelistedAddresses[msg.sender], "Sender already in the whitelist.");
+        require(numAddressesWhitelisted < maxWhitelistedAddresses, "Max limit reached");
+        whitelistedAddresses[msg.sender] = true;
+        numAddressesWhitelisted += 1;
+    }
+
+}
